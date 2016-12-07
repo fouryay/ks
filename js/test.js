c13aa36000bfa672d6f940a0a1521427a7a95743
fouryay <tylernaut@gmail.com>
2016-12-07 17:52:34 -0600

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 id="header">Old Header</h1>
+
+<script>
+var element = document.getElementById("header");
+element.innerHTML = "New Header";
+</script>
+ $("#text").load("../text/flatland1.text"); // element's id should be in string form
+</body>
+</html>
