91b676797c5d049d2bf2fc7832a493e17d3b74d4
fouryay <tylernaut@gmail.com>
2016-12-07 20:59:56 -0600

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,11 +2,8 @@
 <html>
 <body>
 
-<h1 id="header">Old Header</h1>
+<h1 id="header"> Header</h1>
 
-<script>
-var element = document.getElementById("header");
-element.innerHTML = "New Header";
 </script>
  $("#text").load("../text/flatland1.txt"); // element's id should be in string form
 </body>
